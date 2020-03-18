1c0688f4148d1f80a9c39a4a1a0ede104cc6cfb2
212617715 <austin.harris@ge.com>
2020-03-18 04:46:39 -0400

    it work

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # corona-stats
-Fetches COVID19 data from worldometers.info. Why? Because reasons, idk. 
+Fetches COVID19 data from worldometers.info. Why? Because reasons, idfk.
+
+## Why? 
+Uhhhhhh
+
+## What it do
+Gets coronavirus data from worldometers.info.
+
+## What it need 
+- Python3
+- requests==2.22.0
+- beautifulsoup4==4.8.2
+
+## What it look like
+
+```shell
+./fetch.py 
+===== Coronavirus Data =====
+ Coronavirus Cases:  199,306  
+ Deaths:  7,994 
+ Recovered:  82,783 
+============================
+```
+
+## How to use
+
+1. `pip install -r requirements.txt`
+2. `./fetch.py`
+
+## Unit Tests
+lol
+
+## Support
+nah
+
+## Issues 
+Open an issue, sure.
---
+++ fetch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import json
+import os
+import sys
+import requests
+from bs4 import BeautifulSoup 
+
+def parsePage(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    search = soup.find_all("div", {"id": "maincounter-wrap"})
+    cases = search[0].get_text()
+    deaths = search[1].get_text()
+    recovered = search[2].get_text()
+
+    return cases, deaths, recovered
+
+def main(url = 'https://www.worldometers.info/coronavirus/'): 
+    if "https_proxy" in os.environ:
+      proxies = {
+        "https_proxy": os.environ['https_proxy']
+      }
+      r = requests.get(url, proxies=proxies)
+    else:
+      r = requests.get(url)
+    cases, deaths, recovered = parsePage(r.text)
+    print("===== Coronavirus Data =====")
+    print(cases)
+    print(deaths)
+    print(recovered)
+    print("============================")
+
+if __name__ == '__main__':
+  main()
